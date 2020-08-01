11d2a8b1ac8b7d16a754672b2a7c44a510ee07de
luk-tech <68562821+luk-tech@users.noreply.github.com>
2020-08-01 11:01:45 -0400

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This Notebook Will Be Mainly Used For The Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
